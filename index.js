0e3afc0fa8dbb5b240c7c9449b9c8d7d3d66bb89
Igor Kovorotniy <i.kovorotniy@2050.digital>
2020-04-14 17:51:58 +0300

    Added new func, commented some code for debug

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const fetch = require('node-fetch');
 const axios = require('axios').default;
 const cron = require('node-cron');
-postgres_api = require('./postgres.js');
-TB_get_api = require('./TB/get.js');
-TB_HOST = 'localhost'
-TB_PORT = '8080'
-TB_USERNAME = 'tenant@thingsboard.org'
-TB_PASSWORD = 'tenant'
-process.env.POSTGRES_HOST  = 'localhost'
-process.env.POSTGRES_PORT = "5432"
-process.env.POSTGRES_USERNAME = 'postgres'
-process.env.POSTGRES_PASSWORD = 'postgres'
+const postgres_api = require('./postgres.js');
+const TB_get_api = require('./TB/get.js');
+const funcs = require('./functions');
+// TB_HOST = 'localhost'
+// TB_PORT = '8080'
+// TB_USERNAME = 'tenant@thingsboard.org'
+// TB_PASSWORD = 'tenant'
+// process.env.POSTGRES_HOST  = 'localhost'
+// process.env.POSTGRES_PORT = "5432"
+// process.env.POSTGRES_USERNAME = 'postgres'
+// process.env.POSTGRES_PASSWORD = 'postgres'
 
 async function createConnection(options){
     process.env.TB_HOST  = options.TB_HOST;
@@ -25,7 +26,7 @@ async function createConnection(options){
     TB_PORT = process.env.TB_PORT
     TB_USERNAME = process.env.TB_USERNAME;
     TB_PASSWORD = process.env.TB_PASSWORD;
-    await token()
+    // await token()
     await postgres_api.createPostgresConnection();
 }
 
@@ -65,9 +66,93 @@ async function getAndSetToken(options) {
   } 
 }
 
+async function extendChildAttrs(options){
+    const parentId = postgres_api.toPostgresID(options.parent_id);
+    const childId = postgres_api.toPostgresID(options.child_id);
+
+    const parentAttrs = await postgres_api.get.getAttrsAndValuesById(parentId);
+    const childAttrs = await postgres_api.get.getAttrsAndValuesById(childId);
+
+    const parentAttrsValues = funcs.makeAttrsValuesObj(parentAttrs);
+    const childAttrsValues = funcs.makeAttrsValuesObj(childAttrs);
+    const parentKeys = Object.keys(parentAttrsValues);
+
+    for (let parentKey of parentKeys) {
+        // get attribute_types which are not find in child
+        // the write new attribute_keys 
+        if (!(childAttrsValues.hasOwnProperty(parentKey))) {
+            console.log('child not have key: ', parentKey);
+
+            const dataToWrite = parentAttrsValues[parentKey];
+            // set child entity_id, entity_type for extending attributes of child
+            // set null to attributes which not existed before extending of attributes
+            for (let val of dataToWrite) {
+                val.entity_id = childId;
+                val.entity_type = options.child_type;
+                val.bool_v = null;
+                val.str_v = null;
+                val.long_v = null;
+                val.dbl_v = null;
+                val.last_update_ts = Date.now();
+            }
+            try {
+                const result = await postgres_api.insertIntoAttrsKeysVals(dataToWrite);
+
+                if (result.count === dataToWrite.length) {
+                    console.log('successfully write to db!')
+                }
+            } catch (error) {
+                console.error("Insert into err: ", error)
+            }
+            // child and parent have common attributes type SERVER_SCOPE and etc.
+            // we need to find attribute_keys which not presented in child
+        } else {
+            console.log('have common attribute_type ', parentKey);
+
+            let dataToWrite = [];
+            const parentData = parentAttrsValues[parentKey];
+            const childData = childAttrsValues[parentKey];
+
+            for (let i = 0; i < parentData.length; i++) {
+                let match = false;
+                for (let j = 0; j < childData.length; j++) {
+                    if (parentData[i].attribute_key.toString() === childData[j].attribute_key.toString()) {
+                        match = true;
+                        break;
+                    }
+                }
+                if (!match) {
+                    // Change parent properties to child
+                    parentData[i].entity_id = childId;
+                    parentData[i].entity_type = options.child_type;
+                    dataToWrite.push(parentData[i]);
+                }
+            }
+
+            if (dataToWrite.length === 0) {
+                console.log('Not data to write! ');
+                continue;
+            }
+
+            console.log('Find attributes to assing to child !');
+            console.log('data to write ', dataToWrite)
+            try {
+                const result = await postgres_api.insertIntoAttrsKeysVals(dataToWrite);
+
+                if (result.count === dataToWrite.length) {
+                    console.log('successfully write to db!')
+                }
+            } catch (error) {
+                console.error("Insert into err: ", error)
+            }
+
+        }
+    }
+}
 module.exports = {
   get:TB_get_api,
   postgres: postgres_api,
   token: token,
-  createConnection: createConnection
+  createConnection: createConnection,
+  extendChildAttrs: extendChildAttrs,
 };
