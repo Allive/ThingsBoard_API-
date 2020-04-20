02e446854136a7c6cec1e09af790b07b92f557e7
Igor Kovorotniy <i.kovorotniy@2050.digital>
2020-04-20 11:00:10 +0300

    Updated test file for calling extendChildAttrs()

--- test/testExtendAttrs.js
+++ test/testExtendAttrs.js
@@ -1,5 +1,4 @@
 var TB = require("../index.js")
-// const postgres_api = require('../postgres');
 const config = require('dotenv').config();
 
 async function main() {
@@ -7,15 +6,20 @@ async function main() {
         "parent_id": "82c16090-fbe3-11e9-a033-2dde0dc34203",
         "child_id": "aaba34b0-6d07-11ea-94de-3ddf86487a77",
         "child_type": "DEVICE",
+        "attributeKeys": ['entity_type', 'entity_id', 'attribute_type', 'attribute_key', 'bool_v', 'str_v', 'long_v', 'dbl_v', 'last_update_ts'],
+        "updateAttrs": false,
+        TB_HOST: config.parsed.TB_HOST,
+        TB_PORT: config.parsed.TB_PORT,
+        TB_USERNAME: config.parsed.TB_USERNAME,
+        TB_PASSWORD: config.parsed.TB_PASSWORD,
         POSTGRES_HOST: config.parsed.PG_HOST,
         POSTGRES_PORT: config.parsed.PG_PORT,
         POSTGRES_USERNAME: config.parsed.PG_USERNAME,
         POSTGRES_PASSWORD: config.parsed.PG_PASSWORD,
         POSTGRES_DATABASE: config.parsed.PG_DATABASE,
-        "updateAttrs": false,
     }
-   await TB.createConnection(options);
-   await TB.extendChildAttrs(options);
+    await TB.createConnection(options);
+    await TB.extendChildAttrs(options);
 }
 
 main()
